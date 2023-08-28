b6a8830e06e6378f81f28892484cf352137a8c0d
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-08-28 12:16:53 -0600

    Init ToDoListApi

---
+++ ToDoListAPI/Connection/DbConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+
+namespace ToDoListAPI.Connection
+{
+    public class DbConnection
+    {
+        private string _connectionString=string.Empty;
+
+        public DbConnection() { 
+        var constructor= new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            _connectionString = constructor.GetSection("ConnectionStrings:conexionmaestra").Value;
+        }
+
+        public string SqlString() { return _connectionString; }
+
+    }
+}
