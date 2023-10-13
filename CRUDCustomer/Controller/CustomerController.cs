e7706d13c2b35889db7a7e033d95b4faa2889e8e
Ariel-r03 <arieladolfo.silva@gmail.com>
2023-10-12 18:38:41 -0600

    Winform Application, NorthWindDB

---
+++ CRUDCustomer/Controller/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using CRUDCustomer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDCustomer.Controller
+{
+    internal class CustomerController
+    {
+        Customer customer = new Customer();
+        public List<Customer> Customers()
+        {
+            return customer.getCustomer();
+        }
+
+        public void Add(Customer customer)
+        {
+                customer.AddCustomer(customer);
+            
+        }
+
+        public void Delete(string customerID)
+        {
+            if(customerID == null)
+            {
+                MessageBox.Show("El cliente no existe");
+
+            }
+            else
+            {
+                customer.DeleteCustomer(customerID);
+            }
+        }
+
+        public Customer GetCustomer(string customerID)
+        {
+            return customer.getOneCustomer(customerID);
+        }
+
+        public void Update(Customer customer)
+        {
+            customer.UpdateCustomer(customer);
+        }
+    }
+}
